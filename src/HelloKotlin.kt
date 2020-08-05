5162994728c53a52c4d95aabdfd580436759284c
zeyad3ly <zeyadaly15@gmail.com>
2020-08-05 19:35:54 +0200

    Added orientation for variables and data types

--- src/HelloKotlin.kt
+++ src/HelloKotlin.kt
@@ -1,4 +1,17 @@
 fun main() {
-    var userName : String = "Drake."
-    println("Hello world!. My name is $userName")
+    // 1-Variables..
+    val userName = "Zeyad."
+    val age = 23
+    println("Hello world!. My name is $userName I'm $age years old!, in 2 years i'll be ${age + 2} years old!")
+
+    // 2-Data types..
+    val exampleString  = "Name" // String not primitive type
+    val exampleNumber = 2_000_000.12 
+    val exampleLong = exampleNumber.toLong()
+    val exampleChar = '?'
+    val exampleInt : Int  = exampleChar.toInt();
+    val exampleBoolean = false;
+
+
+
 }
